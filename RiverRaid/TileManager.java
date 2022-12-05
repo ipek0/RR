0cb01bf1a5d79fb73cf50b660ecc5380dbb62e95
beyzaasan <105967568+beyzaasan@users.noreply.github.com>
2022-12-05 16:01:34 +0300

    Update TileManager.java

--- RiverRaid/TileManager.java
+++ RiverRaid/TileManager.java
@@ -13,8 +13,8 @@ import main.GamePanel;
 public class TileManager {
 
 	GamePanel gp;
-	Tiles[] tile;
-	int mapTileNumber[][];
+	public Tiles[] tile;
+	public int mapTileNumber[][];
 	
 	public TileManager(GamePanel gp) {
 		
@@ -30,16 +30,19 @@ public class TileManager {
 		try {
 			tile[0]=new Tiles();
 			tile[0].image = ImageIO.read(getClass().getResourceAsStream("grass.png"));
+			tile[0].collision = true;
 			
 			tile[1]=new Tiles();
 			tile[1].image = ImageIO.read(getClass().getResourceAsStream("water.png"));
 			//başka packageden alacaksan /tiles/grass.png
 			
 			tile[2]=new Tiles();
-			tile[2].image = ImageIO.read(getClass().getResourceAsStream("ev.png"));
+			tile[2].image = ImageIO.read(getClass().getResourceAsStream("house.png"));
+			tile[2].collision = true;
 			
 			tile[3]=new Tiles();
 			tile[3].image = ImageIO.read(getClass().getResourceAsStream("tree.png"));
+			tile[3].collision = true;
 			
 		}catch(IOException e) {
 			e.printStackTrace();
@@ -93,8 +96,16 @@ public class TileManager {
 		while(worldCol < gp.maxWorldCol && worldRow < gp.maxWorldRow) {
 			
 			int tileNum=mapTileNumber[worldCol][worldRow];
+			
+			int worldX = worldCol *gp.tileSize;
+			int worldY = worldRow * gp.tileSize;
+			
+			int screenX = worldX - gp.player.worldX + gp.player.screenX;
+			int screenY = worldY - gp.player.worldY + gp.player.screenY;
+			
+			//time --> video 5 14:40
 			//extract a tile number which is stored in mapTileNum[0][0]
-			g2.drawImage(tile[tileNum].image, x, y, gp.tileSize, gp.tileSize, null);
+			g2.drawImage(tile[tileNum].image, screenX, screenY, gp.tileSize, gp.tileSize, null);
 			worldCol++;
 			
 			if(worldCol ==gp.maxScreenCol) {
