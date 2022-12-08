fbe2536994940b50fec27fd9c881775f422d4f9b
beyzaasan <105967568+beyzaasan@users.noreply.github.com>
2022-12-08 14:40:04 +0300

    Update GamePanel.java

--- RiverRaid/GamePanel.java
+++ RiverRaid/GamePanel.java
@@ -9,6 +9,7 @@ import javax.swing.JPanel;
 
 import entity.Player;
 import entity.TileManager;
+import object.SuperObject;
 
 public class GamePanel extends JPanel implements Runnable{
 	// SCREEN SETTINGS
@@ -23,20 +24,22 @@ public class GamePanel extends JPanel implements Runnable{
 	
 	//WORLD SETTINGS
 	public final int maxWorldCol = 16;
-	public final int maxWorldRow = 80;
-	public final int worldWidth = tileSize * maxWorldCol;
-	public final int worldHeight = tileSize * maxWorldRow;
-	
-	
+	public final int maxWorldRow = 160;
 	
 	
 	int FPS = 60;
 	
 	TileManager tileM = new TileManager(this);
 	KeyInputs keyI = new KeyInputs();
+	Sound sound = new Sound();
+	
+	public CollisionChecker cChecker = new CollisionChecker(this);
+	public AssetSetter aSetter = new AssetSetter(this);
 	Thread gameThread;
-	public Player player =new Player(this, keyI);
-	public CollisionChecker cChecker = new CollisionChecker(this); 
+	
+	public Player player =new Player(this, keyI); 
+	public SuperObject[] obj = new SuperObject[10];
+	
 	
 	public GamePanel() {
 		this.setPreferredSize(new Dimension(screenWidth,screenHeight));
@@ -49,6 +52,12 @@ public class GamePanel extends JPanel implements Runnable{
 		this.setFocusable(true);
 		//With this, this GamePanel can be focused to receive key input
 	}
+	
+	public void setupGame() {
+		aSetter.setObject();//to add other setup stuff in future
+		playMusic(0);//tema müziği
+	}
+	
 	
 	public void startGameThread() {
 		gameThread = new Thread(this);
@@ -136,7 +145,17 @@ public class GamePanel extends JPanel implements Runnable{
 		//Graphics2D class extends the graphic class to provide more sophisticaded control
 		//over geometry, coordinate trandformations, color management, and text layout.
 		
+		//TILE
 		tileM.draw(g2);
+		
+		//OBJECT
+		for(int i = 0; i < obj.length; i++) {
+			if(obj[i] != null) {
+				obj[i].draw(g2,this);
+			}
+		}
+		
+		//PLAYER
 		player.draw(g2);
 		
 		g2.dispose();
@@ -144,5 +163,20 @@ public class GamePanel extends JPanel implements Runnable{
 		//resources that is using.
 	}
 	
+	public void playMusic(int i) {
+		
+		sound.setFile(i);
+		sound.play();
+		sound.loop();
+	}
+	
+	public void stopMusic() {
+		sound.stop();
+	}
+	
+	public void playSE(int i) {
+		sound.setFile(i);
+		sound.play();
+	}
+	
 }
-
