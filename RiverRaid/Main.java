62e3434dcac942ad231a8123dfd9ae800eb84cd0
beyzaasan <105967568+beyzaasan@users.noreply.github.com>
2022-12-08 14:40:51 +0300

    Update Main.java

--- RiverRaid/Main.java
+++ RiverRaid/Main.java
@@ -5,6 +5,7 @@ import javax.swing.JFrame;
 public class Main {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
 		JFrame window = new JFrame();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -23,6 +24,7 @@ public class Main {
 		window.setLocationRelativeTo(null);
 		window.setVisible(true);
 		
+		gamepanel.setupGame();
 		gamepanel.startGameThread();
 	}
 
